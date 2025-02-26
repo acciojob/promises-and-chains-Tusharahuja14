26612d1352e5ef8928cdc3b6414c36b2bea7b666
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2025-02-26 16:21:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+let age=document.getElementById("age");
+let name=document.getElementById("name");
+
+let form=document.querSelector("form");
+let submit=document.getElementById("submit");
+
+
+form.preventDefault();
+let promise1=()=>{
+	if(age>18)
+	{
+	return new Promise((resolve,reject)=>{
+		resolve("Welcome, . You can vote.");
+	});
+	}
+	else
+	{
+		reject("Oh sorry . You aren't old enough.");
+	}
+}
+function print()
+{
+
+	if(age.value===""&&name.value==="")
+{
+	alert("Please enter valid details");
+}
+	else
+	{
+		console.log(name,age);
+	}
+}
+
